13d1271ee1e332abf4c356bce926b54779ce1bb3
Sean Philippi <sean.philippi@gmail.com>
2020-02-05 18:37:53 -0600

    pop up removal success, and re-enable scroll success

--- remove-instagram-signup.js
+++ remove-instagram-signup.js
@@ -1,20 +1,25 @@
 // ==UserScript==
 // @name         Remove Instagram Sign Up
-// @namespace
+// @namespace    Violentmonkey Scripts
+// @match       *://*.instagram.com/*
+// @grant       none
 // @version      1.0
 // @description  Hide instagram's singup pop up and layover
 // @author
-// @grant        metadata
 // ==/UserScript==
 const hideInstaLightbox = () => {
-    const { getElementsByClassName } = document;
-    const lightbox = getElementsByClassName('g6RW6');
-    const loginText = getElementsByClassName('_7UhW9');
-    const layover = getElementsByClassName('RnEpo');
+    console.log('boom boom!')
+    const lightbox = document.getElementsByClassName('g6RW6');
+    const loginText = document.getElementsByClassName('_7UhW9');
+    const layover = document.getElementsByClassName('RnEpo');
     const elements = [lightbox, loginText, layover];
     if (lightbox) {
         elements.forEach(el => el[0].setAttribute('style', 'display: none;'));
+        document.body.style.overflow = "scroll";
     }
+    // have a looping check function that checks for the appearance of the overlay
+    // when overlay is spotted and hidden, stop watcher.  hiding the lightbox only needs to happen once
+    // check to make sure any scrolling disabling is reversed as well...
   }
 
   window.setInterval(() => {
