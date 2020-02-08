034766c532140fec0737b76756bbc399cce5b0df
Sean Philippi <sean.philippi@gmail.com>
2020-02-07 18:53:26 -0600

    trying to attach message, footer banner being hidden

--- remove-instagram-signup.js
+++ remove-instagram-signup.js
@@ -1,12 +1,27 @@
 // ==UserScript==
 // @name         Remove Instagram Sign Up
 // @namespace    Violentmonkey Scripts
-// @match       *://*.instagram.com/*
-// @grant       none
+// @match        *://*.instagram.com/*
+// @grant        none
 // @version      1.0
 // @description  Hide instagram's singup pop up and layover
-// @author
+// @author       Sean Philippi
 // ==/UserScript==
+const attachMessage = () => {
+    console.log('attaching')
+    const header = document.querySelector('.NXc7H');
+    const message = document.createElement('DIV');
+    message.innerHTML = '<span>scroll down to see the annoying pop up disappear</span>';
+    header.appendChild(message);
+}
+
+const hideFooterBanner = () => {
+    attachMessage();
+    console.log('be forgotten')
+    const banner = document.getElementsByClassName('N9d2H');
+    banner[0].setAttribute('style', 'display: none;');
+}
+
 const hideInstaLightbox = () => {
     console.log('boom boom!')
     const lightbox = document.getElementsByClassName('g6RW6');
@@ -15,13 +30,16 @@ const hideInstaLightbox = () => {
     const elements = [lightbox, loginText, layover];
     if (lightbox) {
         elements.forEach(el => el[0].setAttribute('style', 'display: none;'));
-        document.body.style.overflow = "scroll";
+        document.body.style.overflow = 'scroll';
     }
     // have a looping check function that checks for the appearance of the overlay
     // when overlay is spotted and hidden, stop watcher.  hiding the lightbox only needs to happen once
     // check to make sure any scrolling disabling is reversed as well...
-  }
+}
 
-  window.setInterval(() => {
+
+window.setTimeout(() => {
     hideInstaLightbox();
-  }, 5000);
+}, 5000);
+
+hideFooterBanner();
